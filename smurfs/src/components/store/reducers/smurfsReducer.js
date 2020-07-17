7c78e93c43f513bcec670e17308c36bece5c454c
Khalil <khalilfoulks@gmail.com>
2020-07-17 11:17:00 -0400

    created store directory added redux to index.js

---
+++ smurfs/src/components/store/reducers/smurfsReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    isLoading: false,
+    smurfs: [],
+    error: "",
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
